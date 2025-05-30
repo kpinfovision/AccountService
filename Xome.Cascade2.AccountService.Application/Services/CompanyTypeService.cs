d1e1062f3c9bf75ab7bbb80c0bcfe945085a8af9
PaluruGuruKumar <Paluru.GuruKumar@infovision.com>
2025-05-30 22:04:22 +0530

    Removed not used code

--- Xome.Cascade2.AccountService.Application/Services/CompanyTypeService.cs
+++ Xome.Cascade2.AccountService.Application/Services/CompanyTypeService.cs
@@ -6,16 +6,11 @@ namespace Xome.Cascade2.AccountService.Application.Services
     public class CompanyTypeService
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly CamundaService _camundaService;
-        private readonly string _connectionString;
-        private readonly ServiceBusPublisher _publisher;
 
-        // , IStateService stateService
+        // ICompanyTypeService companyTypeService
         public CompanyTypeService(IUnitOfWork unitOfWork, CamundaService camundaService, ServiceBusPublisher publisher)
         {
             _unitOfWork = unitOfWork;
-            _camundaService = camundaService;
-            _publisher = publisher;
         }
         public async Task<IEnumerable<CompanyTypes>> GetAllCompanyTypes()
         {
