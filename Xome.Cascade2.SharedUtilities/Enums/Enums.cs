353a32ed9cbcd706993681aca49601a16016d17c
Kiran Patwardhan <Kiran.Patwardhan@infovision.com>
2025-05-30 19:35:52 +0530

    added PaginationModel

--- Xome.Cascade2.SharedUtilities/Enums/Enums.cs
+++ Xome.Cascade2.SharedUtilities/Enums/Enums.cs
@@ -12,4 +12,23 @@
     {
         GENERAL
     }
+
+    //
+    // Summary:
+    //     Specifies how rows of data are sorted.
+    public enum SortOrder
+    {
+        //
+        // Summary:
+        //     The default. No sort order is specified.
+        Unspecified = -1,
+        //
+        // Summary:
+        //     Rows are sorted in ascending order.
+        Ascending = 0,
+        //
+        // Summary:
+        //     Rows are sorted in descending order.
+        Descending = 1
+    }
 }
