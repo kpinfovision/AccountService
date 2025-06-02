544cd9e861655a4a7bf6c883ea63ac83aeb1e7f4
sujatatelang456 <sujata.telang@infovision.com>
2025-06-02 20:25:22 +0530

    changes for org state class

--- Xome.Cascade2.AccountService.Domain/Entities/Company.cs
+++ Xome.Cascade2.AccountService.Domain/Entities/Company.cs
@@ -14,88 +14,82 @@ using System.Text.Json.Serialization;
 
 namespace Xome.Cascade2.AccountService.Domain.Entities
 {
+    [Table("ASSET_MANAGEMENT")]
     public class Company
     {
+        [Column("ASSET_MANAGEMENT_ID")]
         public int CompanyId { get; set; }
-        public int CompanyType { get; set; }
-        public int Status { get; set; }
-        public int RemovedReason { get; set; }
-        public string Address { get; set; }
+        //[JsonIgnore] // not req.
+        //public int CompanyType { get; set; }
+
+        [Column("ACTIVE")]
+        public bool Status { get; set; }
+
+        [JsonIgnore]
+        public int RemovedReason { get; set; } // not on priority
+
+        [Column("NAME")]
+        [Required]
+        [MaxLength(30)]
         public string CompanyName { get; set; }
+        [Column("TAXTYPEID")]
+        public int? TaxIDType { get; set; }
+
+        [Column("TAXID")]
+        [MaxLength(100)]
+        public string? TaxID { get; set; }
+        [Column("NOTES")]
+        [MaxLength(500)]
+        public string? Notes { get; set; }
+
+        [Column("LEGAL_ENTITY_NAME")]
+        [MaxLength(100)]
+        public string? LegalEntityName { get; set; }
+
+        [Column("OUTSOURCE")]
+        [Required]
+        public bool IsOutsourced { get; set; }
+
+        [Column("CREATED_ON")]
+        public DateTime CreatedOn { get; set; }
+
+        [Column("CREATED_BY")]
+        public int CreatedBy { get; set; }
+
+        [Column("MODIFIED_DATE")]
+        public DateTime? ModifiedDate { get; set; }
+
+        [Column("MODIFIED_ON")]
+        public int? ModifiedBy { get; set; }
+        public string DisplayName { get; set; } // need confirmation from PO
+        public string Abbreviation { get; set; } // need confirmation from PO
+        public int[] StateServed { get; set; }
+
+        /// below are attributes of address
+        public string Address { get; set; }
         public string City { get; set; }
+        public int State { get; set; }
+        public string Zip { get; set; }
+        public string Fax { get; set; }
         public string PrimaryPhone { get; set; }
         public string Ext { get; set; }
-        public int TaxIDType { get; set; }
-        public int State { get; set; }
-        public int[] StateServed { get; set; }
-        public string Fax { get; set; }
-        public string TaxID { get; set; }
-        public string Zip { get; set; }
-        public string Notes { get; set; }
-        public string LegalEntityName { get; set; }
-        public string DisplayName { get; set; }
-        public string Abbreviation { get; set; }
+
+        [JsonIgnore]
+        public int AddressId { get; set; }
+
     }
-    //[Table("ASSET_MANAGEMENT")]
-    //public class Company
-    //{
-    //    [Key]
-    //    [Column("ASSET_MANAGEMENT_ID")]
-    //    public int CompanyId { get; set; }
 
-    //    [Column("NAME")]
-    //    [Required]
-    //    [MaxLength(30)]
-    //    public string CompanyType { get; set; }
-
-    //    [Column("STATUS")]
-    //    public int Status { get; set; }
-
-    //    [Column("ADDRESS")]
-    //    [MaxLength(500)]
-    //    public string? Address { get; set; }
-
-    //    [Column("CREATED_ON")]
-    //    public DateTime CreatedOn { get; set; }
-
-    //    [Column("CREATED_BY")]
-    //    public int CreatedBy { get; set; }
-
-    //    [Column("MODIFIED_DATE")]
-    //    public DateTime ModifiedDate { get; set; }
-
-    //    [Column("MODIFIED_ON")]
-    //    public int ModifiedBy { get; set; }
-
-    //    [Column("ACTIVE")]
-    //    public bool IsActive { get; set; }
-
-    //    [Column("TAXTYPEID")]
-    //    public int? TaxTypeId { get; set; }
-
-    //    [Column("TAXID")]
-    //    [MaxLength(100)]
-    //    public string? TaxId { get; set; }
-
-    //    [Column("NOTES")]
-    //    [MaxLength(500)]
-    //    public string? Notes { get; set; }
-
-    //    [Column("LEGAL_ENTITY_NAME")]
-    //    [MaxLength(100)]
-    //    public string? LegalEntityName { get; set; }
-
-    //    [Column("OUTSOURCE")]
-    //    public bool? IsOutsourced { get; set; }
-
-    //    [JsonIgnore]
-    //    [Column("ADDRESS_ID")]
-    //    public int? AddressId { get; set; }
-    //}
-
+    [Table("ORG_STATE")]
     public class CompanyStatesServed
     {
-        public int CompanyId { get; set; }
-        public int StateId { get; set; }
+        [Key]
+        [Column("ORG_STATES_ID")]
+        public int CompanyStatesId { get; set; }
+
+        [Column("ORG_TYPE_SERVICE_ID")]
+        public int? CompanyId { get; set; }
+
+        [Column("STATE_ID")]
+        public int? StateId { get; set; }
     }
 }
